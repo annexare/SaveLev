430ddd2688792b47b5ec31f30f497f577289964d
Dmytro <d@klmn.me>
2022-07-22 19:40:16 +0300

    feat: MUI, menus, icons

---
+++ common.ts
@@ -0,0 +1,6 @@
+export enum ESocialLinks {
+  instagram = 'https://instagram.com/save.lev.sma',
+  instagramMom = 'https://instagram.com/maryklimenko',
+  instagramDad = 'https://instagram.com/nikolaykotlyarchick',
+  telegram = ''
+}
---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document({ __NEXT_DATA__ }: any) {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+          rel="stylesheet"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
