29744c0d8d688540bf22feba84fcc9888979a0a1
Dmytro <d@klmn.me>
2022-07-24 02:14:07 +0300

    feat: add GA

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import Script from 'next/script'
 
 import { NextSeo, SocialProfileJsonLd } from 'next-seo'
 
@@ -272,6 +273,16 @@ const Home: NextPage = () => {
           Powered by <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </a> */}
       </Container>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-BR5JBVCH9L');
+        `}
+      </Script>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-BR5JBVCH9L" strategy="afterInteractive" />
     </>
   )
 }
