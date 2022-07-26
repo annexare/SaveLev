9212506e3ab37a471ad004dccf59b5318d892f70
Dmytro <d@klmn.me>
2022-07-26 03:29:44 +0300

    fix: PayPal button align

--- src/components/PayPal.tsx
+++ src/components/PayPal.tsx
@@ -7,7 +7,11 @@ import paypalIconPath from 'src/assets/paypal.svg'
 import { PAYPAL_DONATE_URL } from 'src/data'
 
 export const PayPalButton: FC<ButtonProps> = (props) => (
-  <Button href={PAYPAL_DONATE_URL} sx={{ backgroundColor: 'rgb(255, 196, 57)' }} {...props}>
+  <Button
+    href={PAYPAL_DONATE_URL}
+    sx={{ alignSelf: 'center', backgroundColor: 'rgb(255, 196, 57)' }}
+    {...props}
+  >
     <Image src={paypalIconPath} alt="PayPal" />
   </Button>
 )
